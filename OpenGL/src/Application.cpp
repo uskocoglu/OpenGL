bd314d0a909f16613998e6b1d197f9d80934a53b
uskocoglu <utku.kocoglu@gmail.com>
2024-08-20 20:54:49 +0300

    Dealing with errors in OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -6,6 +6,29 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR)
+    {
+
+    }
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL ERROR] (" << error << "):" << function << " " <<  file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 struct ShaderProgramSource
 {
     std::string VertexSource;
@@ -137,12 +160,12 @@ int main(void)
 
 
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
     unsigned int ibo;
     glGenBuffers(1, &ibo);
@@ -172,8 +195,11 @@ int main(void)
         //buffer is specified, so its time for OpenGL to draw the triangle. Like a state machine.
         //glDrawArrays(GL_TRIANGLES, 0, 6);
 
+        //to test GL Error Call
+        //GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+
         //draw triangles using Index Buffers
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
